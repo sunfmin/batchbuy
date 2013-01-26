b242c554c1da6f51fb00f1f18f0c938889a0fb41
bom_d_van <bom.d.van@gmail.com>
2013-01-26 16:20:11 +0800

    update api data struct

--- api/data.go
+++ api/data.go
@@ -1,23 +1,23 @@
 package api
 
 type Product struct {
-	Id        string
-	Name      string
-	PhotoLink string
-	Price     int64 // 分
-	ValidFrom string
-	ValidTo string
+    Id        string
+    Name      string
+    PhotoLink string
+    Price     int64 // 分
+    ValidFrom string
+    ValidTo   string
 }
 
 type User struct {
-	Name       string
-	Email      string
-	AvatarLink string
+    Name       string
+    Email      string
+    AvatarLink string
 }
 
 type Order struct {
-	Date     string
-	Product *Product
-	Users     []*User
-	Count    int
+    Date    string
+    Product *Product
+    Users   []*User
+    Count   int
 }
