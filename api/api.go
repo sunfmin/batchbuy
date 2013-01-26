9641fa765252639a62d1c89adabe2c3e81e4968d
bom_d_van <bom.d.van@gmail.com>
2013-01-26 16:19:52 +0800

    change api: one-product-one-order

--- api/api.go
+++ api/api.go
@@ -6,7 +6,9 @@ type Service interface {
 	PutUser(email string, input UserInput) (user *User, err error)
 	RemoveUser(email string) (err error)
 
-	PutOrder(date string, email string, productIds []string) (order *Order, err error)
+	// currently, one product one order support only. multiple products in one order have to be solved by adding multiple orders.
+	// PutOrder(date string, email string, productIds []string) (order *Order, err error)
+	PutOrder(date string, email string, productId string, count int) (order *Order, err error)
 	RemoveOrder(date string, email string)
 
 	ProductListOfDate(date string) (products []*Product, err error)
