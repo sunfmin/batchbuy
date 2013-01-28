2337a5571f84cd3fb0e824600edb36c5a3c322c4
bom_d_van <bom.d.van@gmail.com>
2013-01-28 14:53:07 +0800

    reorganize files

--- model/model.go
+++ model/model.go
@@ -15,7 +15,7 @@ func Init() *mgo.Session {
 	if err != nil {
 		panic(err)
 	}
-	// defer session.Close() // TODO figure out will not closing session causse any problem
+	// defer session.Close() // TODO: figure out will not closing session causse any problem
 	
 	return session
 }
@@ -27,7 +27,7 @@ func Init() *mgo.Session {
 var db = Init().DB("low_tea_at_the_plant")
 
 // Ported from mgo/bson for the sake of not knowing how to update mgo
-// TODO figure out how to update go remote package and remove this function
+// TODO: figure out how to update go remote package and remove this function
 // IsObjectIdHex returns whether s is a valid hex representation of
 // an ObjectId. See the ObjectIdHex function.
 func isObjectIdHex(s string) bool {
