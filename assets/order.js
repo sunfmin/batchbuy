9e103130e9bd204152327e1b665c7b0bd5c8028d
bom_d_van <bom.d.van@gmail.com>
2013-02-19 13:38:02 +0800

    add responses when modifying orders and disable ordering that is older than today

--- assets/order.js
+++ assets/order.js
@@ -5,20 +5,26 @@ $(document).ready(function() {
     // disabled the default enter-key-pressing event on all the forms
     $('form').keypress( function(event){ if (event.which == '13') { event.preventDefault(); } });
     
+    if (moment($('#current-date').html()).isBefore(moment().format('YYYY-MM-DD'))) { 
+        $('button').attr('disabled', true);
+        $('input').attr('disabled', true);
+        return;
+    }
+    
     var getOrder = function(inputField) {
         return {
             email: $('#email').val(),
             date: $('#current-date').html(),
             productid: inputField.parent().parent().find('input[type="hidden"]').val(),
             count: inputField.val()
-        }
+        };
     };
     
-    var putOrder = function(inputField) {
+    var putOrder = function(inputField, doneCallback) {
         $.ajax('/order', {
             type: 'POST',
             data: getOrder(inputField)
-        });
+        }).done(doneCallback);
     };
     
     // on count field changing
@@ -26,11 +32,19 @@ $(document).ready(function() {
         var input = $(this),
             val = parseInt(input.val());
         
+        // var field = $(this);
+        input.attr('data-original-title', 'done');
+        input.tooltip('show');
+        input.attr('data-original-title', '');
+        var doneCallback = function() {
+            setTimeout(function() { input.tooltip('hide'); }, 1000);
+        };
+
         if (val <= 0) { 
             input.val(0);
-            $.ajax('/order?' + $.param(getOrder(input)), { type: 'DELETE' });
+            $.ajax('/order?' + $.param(getOrder(input)), { type: 'DELETE' }).done(doneCallback);
         } else {
-            putOrder(input);
+            putOrder(input, doneCallback);
         }
     });
     
@@ -38,6 +52,14 @@ $(document).ready(function() {
     $('.input-prepend .btn[name="plus"]').on('click', function() {
         var input = $(this).parent().find('input'); 
         input.val(parseInt(input.val()) + 1);
+        
+        var button = $(this);
+        button.attr('data-original-title', 'done');
+        button.tooltip('show');
+        button.attr('data-original-title', '');
+        var doneCallback = function() {
+            setTimeout(function() { button.tooltip('hide'); }, 1000);
+        };
         
         putOrder(input);
     });
@@ -47,40 +69,20 @@ $(document).ready(function() {
         var input = $(this).parent().find('input'),
             val = parseInt(input.val()); 
         
+        var button = $(this);
+        button.attr('data-original-title', 'done');
+        button.tooltip('show');
+        button.attr('data-original-title', '');
+        var doneCallback = function() {
+            setTimeout(function() { button.tooltip('hide'); }, 1000);
+        };
+            
         if (val && val - 1) { 
             input.val(val - 1);
-            putOrder(input);
+            putOrder(input, doneCallback);
         } else {
             input.val(0);
-            $.ajax('/order?' + $.param(getOrder(input)), { type: 'DELETE' });
+            $.ajax('/order?' + $.param(getOrder(input)), { type: 'DELETE' }).done(doneCallback);
         }
     });
-    
-    // put order
-    // $('#submitBtn').click(function() {
-    //     var reloadCount = 0;
-    //     if ($('input[type=email]').val() == '') {
-    //         alert('Please enter your email before submit your orders.');
-    //         return;
-    //     };
-    //     $('input[type="number"]').each(function(index, count) {
-    //         if ($(count).val() != "") {
-    //             reloadCount += 1;
-    //             var data = { email: $('input[type=email]').val()};
-    //             $(count).parent().parent().find('input').each(function(index, input) {
-    //                 data[$(input).attr('name')] = $(input).val();
-    //             });
-    //             
-    //             
-    //             $.ajax('/order', {
-    //                 type: 'POST',
-    //                 data: data
-    //             }).done(function() {
-    //                 reloadCount -= 1;
-    //                 if (reloadCount == 0) {location.reload();};
-    //             });
-    //         };
-    //     });
-    // });
-    
 });
