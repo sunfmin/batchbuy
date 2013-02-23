5b7e0f81e913f76c23a78ed4a4a36027d0972cd4
bom_d_van <bom.d.van@gmail.com>
2013-02-23 15:50:41 +0800

    now copy button is hovering-sensitive

--- assets/order_list.js
+++ assets/order_list.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
-    new ZeroClipboard($("#copy-button"), { moviePath: "/assets/ZeroClipboard.swf" });
+    clip = new ZeroClipboard($("#copy-button"), { 
+        moviePath: "/assets/ZeroClipboard.swf",
+        hoverClass: "bootstrap-btn-hover",
+        activeClass: "bootstrap-btn-active"
+    });
+    // $('#copy-button-view').bind('click', function() {
+    // $('#copy-button').click();
+    // console.log('clicking');
+    // })
 });
