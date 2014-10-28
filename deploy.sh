2c630b2663c4378d7e89b5bf153b3ce41347cb9a
bom_d_van <bom.d.van@gmail.com>
2014-10-28 14:19:16 +0800

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,4 @@
+goxc -bc 'linux'
+scp -p222 batchbuy_linux_amd64.tar.gz lowtea@115.238.41.246:~/batchbuy_linux_amd64.tar.gz
 scp -P222 run_on_cn.sh lowtea@115.238.41.246:/home/lowtea
 ssh -p222 lowtea@115.238.41.246 "chmod +x /home/lowtea/run_on_cn.sh && sudo /home/lowtea/run_on_cn.sh"
--- run_on_cn.sh
+++ run_on_cn.sh
@@ -1,17 +1,27 @@
+#!/usr/bin/env bash
+
+set -e
+
 echo "start run on dev server"
 
-export GOROOT=/opt/go/bin/go
+export GOROOT=/opt/go
 export GOPATH=/home/lowtea/goprojs
 export PATH=$GOROOT/bin:$GOPATH/bin:$PATH
 
 cd $GOPATH/src/github.com/sunfmin/batchbuy
-git pull origin master
-$GOROOT/bin/go install .
+# git pull origin master
+# sudo GOPATH=/home/lowtea/goprojs $GOROOT/bin/go install -a
 
 target="batchbuy"
 echo 'kill running process'
 killall $target;
 
+cd /home/lowtea
+tar mxf batchbuy_linux_amd64.tar.gz
+cp batchbuy_linux_amd64/batchbuy $GOPATH/bin/$target
+
 cd $GOPATH;
 echo 'run in backgroud'
 nohup $GOPATH/bin/$target >> /home/lowtea/goprojs/batchbuy.log 2>&1 &
+
+tail -f /home/lowtea/goprojs/batchbuy.log
